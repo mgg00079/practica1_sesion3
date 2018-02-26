7bcd7af00dfb5e953ad00ca5f6d91d4aee27f72c
Marta <Marta@192.168.0.23>
2018-02-26 20:48:50 +0100

    Se ha creado el formulario, la lista de usuarios, la clase usuario y el jsp de la lista de usuarios

---
+++ WebContent/listausuarios.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Lista de usuarios:</title>
+</head>
+<body>
+<table>
+	<th>Apellidos</th>
+	<th>Nombre</th>
+	<th>email</th>
+	<th>Telefono</th>
+	<th>Codigo Postal</th>
+	
+	<c:forEach items="${usuarios}" var="user" varStatus="estado">
+		<td><c:out value="${user.apellidos}"/></td>
+		<td><c:out value="${user.nombre}"/></td>
+		<td><c:out value="${user.email}"/></td>
+		<td><c:out value="${user.telefono}"/></td>
+		<td><c:out value="${user.cp}"/></td>
+	</c:forEach>
+</table>
+
+</body>
+</html>
+
+<%--https://stackoverflow.com/questions/4928271/how-to-install-jstl-the-absolute-uri-http-java-sun-com-jstl-core-cannot-be-r--%>
---
+++ src/pack/ListaUsuarios.java
@@ -0,0 +1,40 @@
+package pack;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+
+/**
+ * Servlet implementation class ListaUsuarios
+ */
+@WebServlet("/ListaUsuarios")
+public class ListaUsuarios extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ArrayList<Usuario> usuarios = new ArrayList<Usuario> ();
+		Usuario user1 = new Usuario ("Manuel","Casquel Orzaes", "maco0007@ujaen.es", "123456789", "23005");
+		Usuario user2 = new Usuario ("Marta","Gonzalez Gonzalez","mgg00079@ujaen.es", "789456123", "23006");
+		usuarios.add(user1);
+		usuarios.add(user2);
+		request.setAttribute("usuarios", usuarios);
+		String url="/listausuarios.jsp";
+		getServletContext().getRequestDispatcher(url).forward(request, response);
+	
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
